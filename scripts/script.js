96a885b1a2d037a61f9e6431641610fe590b146b
UserGitHub37 <bum37@mail.ru>
2022-03-12 23:30:51 +0300

    feat: create cards from a template

--- scripts/script.js
+++ scripts/script.js
@@ -33,3 +33,85 @@ editButton.addEventListener('click', openPopup);
 formElement.addEventListener('submit', formSubmitHandler);
 
 closeButton.addEventListener('click', closePopup);
+
+
+const popupProfile = document.querySelector('.popup_profile');
+const popupPlace = document.querySelector('.popup_place');
+const popupImage = document.querySelector('.popup_image');
+
+const popupProfileClose = popupProfile.querySelector('.popup__close-button');
+const popupPlaceClose = popupPlace.querySelector('.popup__close-button');
+const popupImageClose = popupImage.querySelector('.popup__close-button');
+
+console.log({popupProfile, popupPlace, popupImage, popupProfileClose, popupPlaceClose, popupImageClose});
+
+/* const togglePopup = function (popup) {
+  popup.classList.toggle('popup_opened');
+}; */
+
+const toggleLike = function (evt) {
+  evt.target.classList.toggle('card__heart_active');
+  console.log(evt.target);
+};
+
+const removeCard = function (evt) {
+  evt.target.closest('.card').remove();
+};
+
+
+const createCard = (data) => {
+  const cardElement = document
+    .querySelector('.card-template')
+    .content.firstElementChild.cloneNode(true);
+
+  const cardHeart = cardElement.querySelector('.card__heart');
+  const cardRemoveButton = cardElement.querySelector('.card__remove-button');
+
+  cardElement.querySelector('.card__title').textContent = data.name;
+  cardElement.querySelector('.card__image').src = data.link;
+  cardElement.querySelector('.card__image').alt = data.name;
+
+  cardHeart.addEventListener('click', toggleLike);
+  cardRemoveButton.addEventListener('click', removeCard);
+// навесить обработчик событий на открытие картинки
+
+  return cardElement;
+};
+
+
+const renderCard = (data, cardsContainer) => {
+  const cardElement = createCard(data);
+  cardsContainer.prepend(cardElement);
+};
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardsContainer = document.querySelector('.elements__wrapper');
+
+initialCards.forEach(card => { renderCard(card, cardsContainer); });
