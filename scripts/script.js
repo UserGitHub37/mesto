636850fceea8bb218220d97e2ddb19d7c9b318bc
UserGitHub37 <bum37@mail.ru>
2022-04-13 18:30:17 +0300

    refactor: Create cards via class

--- scripts/script.js
+++ scripts/script.js
@@ -89,50 +89,7 @@ function openPopupPlace () {
 }
 
 
-function openPopupImage (evt) {
-  const imageDescription = evt.target.parentElement
-    .querySelector('.card__title').textContent;
-  enlargedImage.src = evt.target.src;
-  enlargedImage.alt = imageDescription;
-  imagePopupTitle.textContent = imageDescription;
-  openPopup(imagePopup);
-}
-
-
-function toggleLike (evt) {
-  evt.target.classList.toggle('card__heart_active');
-}
-
-
-function removeCard (evt) {
-  evt.target.closest('.card').remove();
-}
-
-
-function createCard (data) {
-  const cardElement = document
-    .querySelector('.card-template')
-    .content.firstElementChild.cloneNode(true);
-
-  const cardTitle = cardElement.querySelector('.card__title');
-  const cardImage = cardElement.querySelector('.card__image');
-  const cardRemoveButton = cardElement.querySelector('.card__remove-button');
-  const cardHeart = cardElement.querySelector('.card__heart');
-
-  cardTitle.textContent = data.name;
-  cardImage.src = data.link;
-  cardImage.alt = data.name;
-
-  cardImage.addEventListener('click', openPopupImage);
-  cardRemoveButton.addEventListener('click', removeCard);
-  cardHeart.addEventListener('click', toggleLike);
-
-  return cardElement;
-}
-
-
-function renderCard (data, cardsContainer) {
-  const cardElement = createCard(data);
+function renderCard (cardElement, cardsContainer) {
   cardsContainer.prepend(cardElement);
 }
 
@@ -156,7 +113,61 @@ function submitHandlerPlaceForm (evt) {
 }
 
 
-initialCards.forEach(cardData => { renderCard(cardData, cardsContainer); });
+class Card {
+  constructor(cardData, templateSelector) {
+    this._cardData = cardData;
+    this._templateSelector = templateSelector;
+  }
+
+
+  _openPopupImage () {
+    const imageDescription = this._cardTitle.textContent;
+    enlargedImage.src = this._cardImage.src;
+    enlargedImage.alt = imageDescription;
+    imagePopupTitle.textContent = imageDescription;
+    openPopup(imagePopup);
+  }
+
+
+  _toggleLike () {
+    this._cardHeart.classList.toggle('card__heart_active');
+  }
+
+
+  _removeCard () {
+    this._cardElement.remove();
+  }
+
+
+  createCard () {
+    this._cardElement = document
+      .querySelector(this._templateSelector)
+      .content.firstElementChild.cloneNode(true);
+
+    const cardRemoveButton = this._cardElement.querySelector('.card__remove-button');
+    this._cardTitle = this._cardElement.querySelector('.card__title');
+    this._cardImage = this._cardElement.querySelector('.card__image');
+    this._cardHeart = this._cardElement.querySelector('.card__heart');
+
+    this._cardTitle.textContent = this._cardData.name;
+    this._cardImage.src = this._cardData.link;
+    this._cardImage.alt = this._cardData.name;
+
+    cardRemoveButton.addEventListener('click', () => this._removeCard());
+    this._cardImage.addEventListener('click', () => this._openPopupImage());
+    this._cardHeart.addEventListener('click', () => this._toggleLike());
+
+    return this._cardElement;
+  }
+
+}
+
+
+initialCards.forEach(cardData => {
+  const card = new Card(cardData, '.card-template');
+  const cardElement = card.createCard();
+  renderCard(cardElement, cardsContainer);
+});
 
 
 popups.forEach((popup) => {
